68662ce2e09ba3fc6aaef36b19242415f2078106
pranita-raut <pranita.raut@aptaracorp.com>
2026-06-30 23:03:58 +0530

    2 new done
    2 new done

--- OEBPS/widgets/Case_widgets/Case_9_10/case9_7/js/preloader.js
+++ OEBPS/widgets/Case_widgets/Case_9_10/case9_7/js/preloader.js
@@ -16,19 +16,23 @@ var imgPreloadArray = new Array(
 	"images/table_btn.png",
 	"images/top_patch.png",	
 	
-	"images/q1/on001-ca005f001-9780323640251.png",
-	"images/q1/on001-ca005f001-9780323640251-a.png",
-	"images/q1/on001-ca005f001-9780323640251-b.png",
-	"images/q3/on001-ca005f001-9780323640251.png",
-	"images/q3/on001-ca005f001-9780323640251-a.png",
-	"images/q3/on001-ca005f001-9780323640251-b.png",
-	"images/q3/on001-ca005f001-9780323640251-c.png",
-	"images/q3/on001-ca005f003-9780323640251.png",
-	"images/q3/on001-ca005f003-9780323640251-a.png",
-	"images/q4/on001-ca005f004-9780323640251.png",
-	"images/q4/on001-ca005f004-9780323640251-a.png",
-	"images/q4/on001-ca005f005-9780323640251.png",
-	"images/q4/on001-ca005f005-9780323640251-a.png"
+	"images/q3/on003-ca002f001-9780323640251.png",
+	"images/q3/on003-ca002f002-9780323640251.png",
+	"images/q6/on003-ca002f002-9780323640251.png",
+	"images/q11/on003-ca002f003-9780323640251.png",
+	"images/q11/on003-ca002f003-9780323640251-a.png",
+	"images/q14/on003-ca002f004-9780323640251.png",
+	"images/q14/on003-ca002f004-9780323640251-a.png",
+	"images/q15/on003-ca002f005-9780323640251.png",
+	"images/q15/on003-ca002f005-9780323640251-a.png",
+	"images/q15/on003-ca002f005-9780323640251-b.png",
+	"images/q15/on003-ca002f006-9780323640251.png",
+	"images/q15/on003-ca002f006-9780323640251-a.png",
+	"images/q15/on003-ca002f006-9780323640251-b.png",
+	"images/q15/on003-ca002f006-9780323640251-c.png",
+	"images/q16/on003-ca002f007-9780323640251.png",
+	"images/q16/on003-ca002f007-9780323640251-a.png",
+	"images/q16/on003-ca002f007-9780323640251-b.png"
 	);
 var imagePreCount = 0;
 for(var pId = 0; pId < imgPreloadArray.length; pId++)
