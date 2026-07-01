2e9e786f4659f05e315691a22d4bb20a32d5bd02
pranita-raut <pranita.raut@aptaracorp.com>
2026-07-01 20:42:30 +0530

    some changes done
    some changes done

---
+++ OEBPS/widgets/Case_widgets/case15_5/js/preloader.js
@@ -0,0 +1,59 @@
+var imgPreloadArray = new Array(
+	"images/arrow.png",
+	"images/bottom_patch.png",
+	"images/check_btn.png",	
+	"images/checkbox.png",
+	"images/closeicon.png",
+	"images/dot.png",
+	"images/gray_dot.png",
+	"images/loading.gif",
+	"images/menu_btn.png",
+	"images/next_btn.png",
+	"images/next_btn.png",
+	"images/placeholder.png",
+	"images/placeholder.jpg",
+	"images/prev_btn.png",
+	"images/table_btn.png",
+	"images/top_patch.png",	
+	
+	"images/q1/on015-ca004f001-9780323640251.png",
+	"images/q1/on015-ca004f002-9780323640251.png",
+	"images/q1/on015-ca004f002-9780323640251-a.png",
+	"images/q1/on015-ca004f002-9780323640251-b.png",
+	"images/q1/on015-ca004f002-9780323640251-c.png",
+	"images/q2/on015-ca004f002-9780323640251.png",
+	"images/q2/on015-ca004f002-9780323640251-a.png",
+	"images/q2/on015-ca004f002-9780323640251-b.png",
+	"images/q2/on015-ca004f002-9780323640251-C-new-replacement.png",
+	"images/q3/on015-ca004f003-9780323640251.png",
+	"images/q3/on015-ca004f003-9780323640251-a.png",
+	"images/q3/on015-ca004f003-9780323640251-b.png",
+	"images/q3/on015-ca004f004-9780323640251.png",
+	"images/q3/on015-ca004f004-9780323640251-a.png",
+	"images/q3/on015-ca004f004-9780323640251-b.png",
+	"images/q5/on015-ca004f005-9780323640251.png",
+	"images/q5/on015-ca004f005-9780323640251-a.png",
+	"images/q5/on015-ca004f005-9780323640251-b.png",
+	"images/q12/on015-ca004f006-9780323640251.png",
+	"images/q12/on015-ca004f006-9780323640251-a.png",
+	"images/q12/on015-ca004f006-9780323640251-b.png",
+	"images/q12/on015-ca004f006-9780323640251-c.png",
+	"images/q12/on015-ca004f007-9780323640251.png",
+	"images/q12/on015-ca004f007-9780323640251-a.png",
+	"images/q13/on015-ca004f008-9780323640251.png",
+	"images/q13/on015-ca004f008-9780323640251-a.png",
+	"images/q13/on015-ca004f008-9780323640251-b.png",
+	"images/q14/on015-ca004f009-9780323640251.png",
+	"images/q14/on015-ca004f009-9780323640251-a.png"
+	);
+var imagePreCount = 0;
+for(var pId = 0; pId < imgPreloadArray.length; pId++)
+{
+	var img = new Image();
+	img.onload = imagePreloaded;
+	img.src = imgPreloadArray[pId];
+}
+function imagePreloaded()
+{
+	imagePreCount++;
+}
